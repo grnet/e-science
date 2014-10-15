8046c5de42b2fd05d7001771aaa83dbc23182f5d
gtzelepis <gtzelepis@allweb.gr>
2014-10-15 15:29:27 +0300

    update .gitignore to add .idea subfolder skeleton testcase using unittest and mock for create_bare_cluster.create_cluster()

--- tests/test_create_cluster.py
+++ tests/test_create_cluster.py
@@ -1,6 +1,36 @@
+# setup testing framework
 from unittest import TestCase
+from nose.tools import assert_equal,assert_raises
+from mock import Mock,patch
+
+# get relative path references so imports will work,
+# even if __init__.py is missing (/tests is a simple directory not a module)
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# import objects we aim to test
+from create_bare_cluster import create_cluster
+from okeanos_utils import Cluster
 
 
 class TestCreate_cluster(TestCase):
+    # initialize objects common to all tests in this test case
+    def setUp(self):
+        self.mock_cluster = Mock(Cluster)
+
     def test_create_cluster(self):
-        self.fail()
+        # arrange
+        self.mock_cluster.create.return_value = "127.0.0.1"
+        # act
+        # cluster = create_cluster() #"test",2,2,4096,5,'ext_vlmc',2,2048,5,'xAFj2zGeNeRl-JbeSM55OMdpjh_1dYmXSrGB7PmTbhA','Debian Base')
+        cluster = self.mock_cluster.create()
+        # assert
+        self.assertEqual(cluster,"127.0.0.1")
+
+    # free up any resources not automatically released
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
