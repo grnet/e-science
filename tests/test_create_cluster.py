46788660970b5af8b03221d2fe73b472e400cf0d
gtzelepis <gtzelepis@allweb.gr>
2014-10-20 12:45:28 +0300

    - reformat (no functional changes) to stop pylint nagging me

--- tests/test_create_cluster.py
+++ tests/test_create_cluster.py
@@ -1,11 +1,12 @@
 # setup testing framework
-from unittest import TestCase,main
+from unittest import TestCase, main
 from mock import patch
 
 # get relative path references so imports will work,
 # even if __init__.py is missing (/tests is a simple directory not a module)
 import sys
 import os.path
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 # import objects we aim to test
@@ -18,13 +19,16 @@ def mock_createcluster(*args):
     fakevmdict = {1: 'f vm'}
     return fakeip, fakevmdict
 
+
 def mock_sleep(*args):
     print 'in mock sleep'
     pass
 
+
 def mock_checkcredentials(*args):
     print 'in mock check credentials'
     pass
+
 
 def mock_endpoints_userid(arg1):
     print 'in mock endpoints'
@@ -32,28 +36,35 @@ def mock_endpoints_userid(arg1):
     fakeendpoints = {'cyclades': 0, 'plankton': 0, 'network': 0}
     return fakeendpoints, fakeuid
 
+
 def mock_init_cyclades(*args):
     print 'in mock cyclades'
+
 
 def mock_get_flavorid(*args):
     print 'in mock flavor id'
     return 1
 
+
 def mock_check_quota(*args):
     print 'in mock check quota'
 
+
 class mock_plankton():
-    def list_public(self,*args):
-        return [{'name':'','id':0}]
+    def list_public(self, *args):
+        return [{'name': '', 'id': 0}]
+
 
 def mock_init_plankton(*args):
     print 'in mock init plankton'
     plist = mock_plankton()
     return plist
 
+
 def mock_init_cyclades_netclient(*args):
     print 'in mock init cyclades netclient'
     pass
+
 
 # replace unmanaged calls with the mocks
 @patch('create_bare_cluster.Cluster.create', mock_createcluster)
@@ -75,15 +86,16 @@ class TestCreateCluster(TestCase):
         fakeip = '127.0.0.1'
         fakevmdict = {1: 'f vm'}
         # act
-        retip, retvmdict = create_cluster('',0,0,0,0,'',0,0,0,'','')
+        retip, retvmdict = create_cluster('', 0, 0, 0, 0, '', 0, 0, 0, '', '')
         # assert
-        self.assertEqual((fakeip,fakevmdict),(retip,retvmdict))
+        self.assertEqual((fakeip, fakevmdict), (retip, retvmdict))
 
     # more tests go here
 
     # free up any resources not automatically released
     def tearDown(self):
         pass
+
 
 if __name__ == '__main__':
     main()
