9ee0cf19207e2195529bd482d5bf563176adb37e
ioannisstenos <johnstenos83@gmail.com>
2014-10-23 14:24:15 +0100

    E-science login/logout functionality with Ember.js

---
+++ ember_django/backend/serializers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Serializers file for django rest framework.
+
+@author: Ioannis Stenos, Nick Vrionis
+'''
+
+from rest_framework import serializers
+from backend.models import UserInfo
+from backend.models import ClusterInfo
+
+
+class OkeanosTokenSerializer(serializers.Serializer):
+    '''Serializer for okeanos token from ember login.'''
+    token = serializers.CharField()
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    '''
+    Serializer for UserInfo object with cluster and escience_token
+    added fields.
+    '''
+    cluster = serializers.SerializerMethodField('number_of_clusters')
+    escience_token = serializers.RelatedField()
+
+    class Meta:
+        model = UserInfo
+        fields = ('user_id', 'cluster', 'escience_token')
+
+    def number_of_clusters(self, obj):
+        '''
+        Function that calculates the number of clusters of a UserInfoinstance.
+        '''
+        clusters = ClusterInfo.objects.all().filter(user_id=obj.user_id). \
+            filter(cluster_status=1).count()
+        return clusters
