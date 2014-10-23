9ae708f7187c2d054f99e33e99d72e59a0fec14e
ioannisstenos <johnstenos83@gmail.com>
2014-10-23 17:09:40 +0100

    e-science login/logout functionality with ember and django backend

--- ember_django/app/js/app.js
+++ ember_django/app/js/app.js
@@ -154,9 +154,13 @@ App.LogoutRoute = Ember.Route.extend({
     var current_user = this.store.update('user', {'id': 1}).save();
     current_user.then(
     function(){
-        //Set global var escience and localStorage token to null.
+        //Set global var escience and localStorage token to null when put is successful.
     	App.set('escience_token', "null");
         window.localStorage.escience_auth_token = App.get('escience_token');
+    }, function(){
+     //Set global var escience and localStorage token to null when put fails.
+    	App.set('escience_token', "null");
+        window.localStorage.escience_auth_token = App.get('escience_token'); 
     });
     this.transitionTo('homepage');
 }  
