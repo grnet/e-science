30575fd744f61f8f8542db6aa3f2b0e4980f575f
gtzelepis <gtzelepis@allweb.gr>
2014-11-24 15:57:00 +0200

    templates moved out of index.html to separate handlebars files, template loader defaults adjusted for project root

--- ember_django/frontend/app/libs/ember-handlebars-loader-0.0.1.js
+++ ember_django/frontend/app/libs/ember-handlebars-loader-0.0.1.js
@@ -2,7 +2,7 @@ var EmberHandlebarsLoader = {
   loadTemplates: function(templateNames) {
     templateNames.forEach(function(name) {
       $.ajax({
-        url: "templates/" + name + ".hbs",
+        url: "frontend/app/templates/" + name + ".hbs",
         async: false,
         success: function(template) {
           var compiledTemplate = Ember.Handlebars.precompile(template);
