19c7e3e13be04c106135c61bdc7d63516fd79b6e
ioannisstenos <johnstenos83@gmail.com>
2014-11-29 07:58:14 +0000

    Refactored create_cluster with refactored frontend

--- ember_django/frontend/app/routes/cluster_create.js
+++ ember_django/frontend/app/routes/cluster_create.js
@@ -1,7 +1,12 @@
 // Cluster/create route (/cluster/create url)
 App.ClusterCreateRoute = App.RestrictedRoute.extend({
+    needs: 'clusterCreate',
     // model for create cluster choices (input form)
     model : function() {
 	return this.store.find('cluster', 1);
-    }
+    },
+    afterModel : function() {
+	// resets variables every time you go to the create cluster
+	this.controllerFor('clusterCreate').reset_variables();
+    },
 });
