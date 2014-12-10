34249a5a0b234521fb9b5f2196ac64933d595d63
gtzelepis <gtzelepis@allweb.gr>
2014-12-10 12:48:30 +0200

    hook up rest adapter, comment out fixtures

--- ember_django/frontend/app/models/user.js
+++ ember_django/frontend/app/models/user.js
@@ -13,39 +13,40 @@ App.User = DS.Model.extend({
 });
 
 App.UserCluster = DS.Model.extend({
-	cluster_name : attr('string'), // name of the cluster
-	cluster_size : attr('number'), // size of cluster (master+slaves)
-	cluster_status : attr('string'), // status of cluster
-	user : DS.belongsTo('user'), // user that created the cluster
+	cluster_name : attr('string'), 		// name of the cluster
+	cluster_size : attr('number'), 		// size of cluster (master+slaves)
+	cluster_status : attr('string'), 	// status of cluster
+	master_IP : attr('string'),			// master ip
+	user : DS.belongsTo('user'), 		// user that created the cluster
 });
 
-App.User.reopenClass({
-	FIXTURES : [{
-		id : 1,
-		token : "PLACEHOLDER",
-		cluster : 3,
-		clusters : [1, 2, 3]
-	}]
-});
-
-App.UserCluster.reopenClass({
-	FIXTURES : [{
-		id : 1,
-		cluster_name : "acluster",
-		cluster_size : 5,
-		cluster_status : "PENDING",
-		user : 1
-	}, {
-		id : 2,
-		cluster_name : "fcluster",
-		cluster_size : 3,
-		cluster_status : "ACTIVE",
-		user : 1
-	}, {
-		id : 3,
-		cluster_name : "ecluster",
-		cluster_size : 12,
-		cluster_status : "DESTROYED",
-		user : 1
-	}]
-});
+// App.User.reopenClass({
+	// FIXTURES : [{
+		// id : 1,
+		// token : "PLACEHOLDER",
+		// cluster : 3,
+		// clusters : [1, 2, 3]
+	// }]
+// });
+// 
+// App.UserCluster.reopenClass({
+	// FIXTURES : [{
+		// id : 1,
+		// cluster_name : "acluster",
+		// cluster_size : 5,
+		// cluster_status : "PENDING",
+		// user : 1
+	// }, {
+		// id : 2,
+		// cluster_name : "fcluster",
+		// cluster_size : 3,
+		// cluster_status : "ACTIVE",
+		// user : 1
+	// }, {
+		// id : 3,
+		// cluster_name : "ecluster",
+		// cluster_size : 12,
+		// cluster_status : "DESTROYED",
+		// user : 1
+	// }]
+// });
--- ember_django/frontend/app/store.js
+++ ember_django/frontend/app/store.js
@@ -7,14 +7,14 @@
 
 // Extend Application Adapter settings for Token Authentication and REST calls to /api
 // Changes of global var escience_token are reflected in the Authorization header of our REST calls
-// App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-	// namespace : 'api',
-	// headers : function() {
-		// return {
-			// "Authorization" : App.escience_token
-		// };
-	// }.property("App.escience_token")
-// }); 
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
+	namespace : 'api',
+	headers : function() {
+		return {
+			"Authorization" : App.escience_token
+		};
+	}.property("App.escience_token")
+}); 
 
 // For fixtures
-App.ApplicationAdapter = DS.FixtureAdapter;
+// App.ApplicationAdapter = DS.FixtureAdapter;
