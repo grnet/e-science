8566c8ff3e13b22cf241c7ca241f08a5817142ee
gtzelepis <gtzelepis@allweb.gr>
2014-12-11 18:41:19 +0200

    userclusters table sorts

--- ember_django/frontend/app/controllers/user_welcome.js
+++ ember_django/frontend/app/controllers/user_welcome.js
@@ -5,14 +5,23 @@ App.UserWelcomeController = Ember.Controller.extend({
 	needs : 'clusterCreate',
 	output_message : '', // output message of create cluster script
 	create_cluster_start : false, // flag to see if the transition is from create cluster button
+
+	sortedclusters : [],
+	column : '',
 	sortdir : false,
+	sortedCollection: function() {
+		return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+			content: this.get('sortedclusters'),
+			sortProperties: [this.get('column')],
+			sortAscending: this.get('sortdir')
+		});
+	}.property('sortdir'),
 	actions : {
 		sortBy : function(clusters, column) {
-			// console.log(clusters);
-			// console.log(column);
-			clusters.set('sortProperties', [column]);
+			this.set('sortedclusters', clusters);
+			this.set('column', column);
 			this.set('sortdir', !this.get('sortdir'));
-			clusters.set('sortAscending', this.get('sortdir'));
-		},
+			console.log(String(this.get(sortdir)));
+		}
 	},
 });
