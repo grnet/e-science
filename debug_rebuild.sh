d87e3f5c06109256bf798e1de1e18ee56d5c215d
gtzelepis <gtzelepis@allweb.gr>
2015-01-30 02:19:42 +0200

    improve project rebuild bash script (debug)

--- debug_rebuild.sh
+++ debug_rebuild.sh
@@ -6,7 +6,7 @@ echo 'done'
 echo 'delete *.pyc'
 sudo find . -name "*.pyc" -type f -delete
 echo 'done'
-echo 'rebuilding orka'
+echo 'rebuild orka'
 cd orka-0.1.1
 sudo rm -rf build/
 sudo rm -rf dist/
@@ -14,21 +14,21 @@ sudo rm -rf orka.egg-info/
 sudo python setup.py install
 echo 'done'
 cd ../ember_django
-echo 'stopping uWSGI if running'
+echo 'stop uWSGI if running'
 sudo killall -s INT uwsgi
 echo 'done'
-echo 'stopping nginx if running'
+echo 'stop nginx if running'
 sudo /etc/init.d/nginx stop
 echo 'done'
-echo 'stopping celery'
+echo 'stop celery'
 celery multi stopwait celeryworker1 --loglevel=INFO --app=backend.celeryapp --pidfile=/tmp/\%n.pid --logfile=$HOME/logs/\%n\%I.log
 echo 'done'
-echo 'restarting rabbitmq'
+echo 'restart rabbitmq'
 sudo /etc/init.d/rabbitmq-server restart
 echo 'done'
-echo 'starting celery'
+echo 'start celery'
 celery multi start celeryworker1 --loglevel=INFO --app=backend.celeryapp --pidfile=/tmp/\%n.pid --logfile=$HOME/logs/\%n\%I.log
 echo 'done'
-echo 'starting django test server'
+echo 'start django test server'
 python manage.py runserver
 
