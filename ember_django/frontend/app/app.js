936cde0657a43ba27ca2b3a3e6d09aff0dcd450e
nvrionis <nick.vrionis@gmail.com>
2015-03-20 17:54:52 +0200

    Upload and read file

--- ember_django/frontend/app/app.js
+++ ember_django/frontend/app/app.js
@@ -21,4 +21,29 @@ App.attr = DS.attr;
 var escience_token;
 App.set('escience_token', "null");
 
+App.TextFileUpload = Ember.TextField.extend({
+  type: 'file',
+  attributeBindings: ['name'],
+  change: function (evt) {
+    var files = evt.target.files;
+    if (!files.length) {
+      alert('Please select a file!');
+      return;
+    }
+    var file = files[0];
+    var reader = new FileReader();
+    var core_site;	
+    // If we use onloadend, we need to check the readyState.
+    reader.onload = (function(theFile) {
+    	 return function(e) {
+          // Render thumbnail.
+          alert('ok');
+          core_site = e.target.result;
+          console.log(core_site);
+        };
+      })(file);
+    return reader.readAsText(file);
+    }	
+});
+
 
