bcfd0a04d1009d3d70535e78868d779510ea90d6
ioannisstenos <johnstenos83@gmail.com>
2015-03-26 08:18:38 -0400

    Revert to project status pre-upload xml task

--- ember_django/backend/settings.py
+++ ember_django/backend/settings.py
@@ -128,7 +128,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
