2e8385f6fafa671d3e2b5923f09d761f558433c9
nvrionis <nick.vrionis@gmail.com>
2015-04-15 18:24:46 +0300

    Cluster managment cluster and hadoop actions

--- ember_django/frontend/app/routes/cluster_management.js
+++ ember_django/frontend/app/routes/cluster_management.js
@@ -17,8 +17,6 @@ App.ClusterManagementRoute = App.RestrictedRoute.extend({
 				for (var i = 0; i < length; i++) {
 					if (clusters.objectAt(i).get('cluster_name') == params["usercluster.cluster_name"])
 					{
-						self.set('sel_cluster', clusters.objectAt(i));
-						//alert(clusters.objectAt(i).get('cluster_name'));
 					 	return clusters.objectAt(i);
 					}
 				}
@@ -27,10 +25,86 @@ App.ClusterManagementRoute = App.RestrictedRoute.extend({
  			}, function(reason) {
 				console.log(reason.message);
 			});
+ 	return selected_cluster;
+	},
+	
+	actions: {
 		
-
+		takeAction : function(cluster) {
+			var self = this;
+			var store = this.store;
+			var action = cluster.get('cluster_confirm_action');
+			cluster.set('cluster_confirm_action', false);
+			switch(action) {
+			case 'cluster_delete':
+				cluster.destroyRecord().then(function(data) {
+					var count = self.controller.get('count');
+					var extend = Math.max(5, count);
+					self.controller.set('count', extend);
+					self.controller.set('create_cluster_start', true);
+					self.controller.send('timer', true, store);
+				}, function(reason) {
+					console.log(reason.message);
+					if (!Ember.isBlank(reason.message)){
+						var msg = {'msg_type':'danger','msg_text':reason.message};
+                        self.controller.send('addMessage',msg);
+					}
+				});
+				break;
+			case 'hadoop_start':
+				cluster.set('hadoop_status','start');
+				cluster.save().then(function(data){
+					var count = self.controller.get('count');
+					var extend = Math.max(5, count);
+					self.controller.set('count', extend);
+					self.controller.set('create_cluster_start', true);
+					self.controller.send('timer', true, store);
+				},function(reason){
+					console.log(reason.message);
+					if (!Ember.isBlank(reason.message)){
+						var msg = {'msg_type':'danger','msg_text':reason.message};
+                        self.controller.send('addMessage',msg);
+					}
+				});
+				break;
+			case 'hadoop_stop':
+				cluster.set('hadoop_status','stop');
+				cluster.save().then(function(data){
+					var count = self.controller.get('count');
+					var extend = Math.max(5, count);
+					self.controller.set('count', extend);
+					self.controller.set('create_cluster_start', true);
+					self.controller.send('timer', true, store);
+				},function(reason){
+					console.log(reason.message);
+					if (!Ember.isBlank(reason.message)){
+						var msg = {'msg_type':'danger','msg_text':reason.message};
+                        self.controller.send('addMessage',msg);
+					}
+				});
+				break;
+			case 'hadoop_format':
+				cluster.set('hadoop_status','format');
+				cluster.save().then(function(data){
+					var count = self.controller.get('count');
+					var extend = Math.max(5, count);
+					self.controller.set('count', extend);
+					self.controller.set('create_cluster_start', true);
+					self.controller.send('timer', true, store);
+				},function(reason){
+					console.log(reason.message);
+					if (!Ember.isBlank(reason.message)){
+						var msg = {'msg_type':'danger','msg_text':reason.message};
+                        self.controller.send('addMessage',msg);
+					}
+				});
+				break;
+			}
+		},
 		
-	 	return selected_cluster;
+		confirmAction : function(cluster, value) {
+			cluster.set('cluster_confirm_action', value);
+		}	
 	}
 	  
 });
