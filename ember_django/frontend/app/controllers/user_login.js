d68e4e91715c0e3e809aff9d4cba4932be376f0e
KPetsas <kwsthsik8@gmail.com>
2015-04-22 04:16:43 -0400

    Logout after 1h

--- ember_django/frontend/app/controllers/user_login.js
+++ ember_django/frontend/app/controllers/user_login.js
@@ -72,7 +72,7 @@ App.UserLoginController = Ember.Controller.extend({
 			self.set('controllers.application.loggedIn', false);
 			self.set('token', '');
 			self.transitionToRoute('user.logout');
-        }, 60000));
+        }, 3600000));
 	},
 	
 	cancelRunTimer : function(){
