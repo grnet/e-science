38f39050ed6717dfe8413031b3f4a94422a250f5
KPetsas <kwsthsik8@gmail.com>
2015-05-26 05:37:10 -0400

    Ansible corrections on hadoop Ecosystem

--- ember_django/backend/run_ansible_playbooks.py
+++ ember_django/backend/run_ansible_playbooks.py
@@ -53,8 +53,8 @@ def install_yarn(*args):
         if args[4] == 'hue':
             ansible_manage_cluster(cluster_id, 'HUEstart')
         if args[4] == 'ecosystem':
-            ansible_manage_cluster(cluster_id, 'HUEstart')
             ansible_manage_cluster(cluster_id, 'ECOSYSTEMstart')
+            ansible_manage_cluster(cluster_id, 'HUEstart')
     except Exception, e:
         msg = 'Error while running Ansible '
         raise RuntimeError(msg, error_ansible_playbook)
