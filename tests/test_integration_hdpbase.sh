a4e1bbf5e356a0df8caa8ced07e80f3e84c73214
gtzelepis <gtzelepis@allweb.gr>
2015-06-05 15:13:16 +0300

    Orka CLI modifications to make it more testable (some local stdout returns), integration test re-written to leverage shunit2, implement test suite skipping based on env variable toggle (can be set on Travis CI per account and repo)

--- tests/test_integration_hdpbase.sh
+++ tests/test_integration_hdpbase.sh
@@ -1,120 +1,163 @@
-#!/bin/bash
+#! /bin/bash
+# file: tests/test_integration_hdpbase.sh
 
-# 01. Create
-# 02. Stop
-# 03. Format
-# 04. Start
+# shunit2 compatible unittest file, requires shunit2 installed (apt-get install shunit2)
+
+# Tests
+# 01. Create Cluster
+# 02. Stop Hadoop
+# 03. Format Hadoop
+# 04. (Re)Start Hadoop
 # 05. runPI
 # 06. wordcount
 # 07. teragen
-# 08. pithosFS registered
+# 08. pithosFS is registered
 # 09. runPI pithosFS
 # 10. wordcount pithosFS
 # 11. teragen pithosFS
-# 12. Destroy
+# 12. Destroy Cluster
 
-# setUp
-setUp(){
+
+oneTimeSetUp(){
+	# runs before whole test suite
+	STAGING_IP=http://83.212.115.45
 	local OKEANOS_TOKEN=$(cat .private/.config.txt | grep "token" |cut -d' ' -f3)
-	echo -e '[global]\ndefault_cloud = ~okeanos\nignore_ssl = on\n[cloud "~okeanos"]\nurl = https://accounts.okeanos.grnet.gr/identity/v2.0\ntoken = '$OKEANOS_TOKEN'\n[orka]\nbase_url = http://83.212.115.45' > ~/.kamakirc
+	echo -e '[global]\ndefault_cloud = ~okeanos\nignore_ssl = on\n[cloud "~okeanos"]\nurl = https://accounts.okeanos.grnet.gr/identity/v2.0\ntoken = '$OKEANOS_TOKEN'\n[orka]\nbase_url = '$STAGING_IP > ~/.kamakirc	
 }
-tearDown(){
+
+oneTimeTearDown(){
+	# runs after whole test suite
 	rm -f ~/.kamakirc
 	rm -f _tmp.txt
 	unset SSHPASS
 }
 
-setUp
-# 01 Create
-declare -a ARR_RESULT=($(orka create integration_test 2 2 2048 5 2 2048 5 standard escience.grnet.gr 2 128 --use_hadoop_image))
-CLUSTER_ID=${ARR_RESULT[1]}
-MASTER_IP=${ARR_RESULT[3]}
-export SSHPASS=${ARR_RESULT[5]}
-if [ -n "$CLUSTER_ID" ]; then
-	printf "Create Cluster: OK\n"
-	echo 0 >&1
-else
-	printf "Create Cluster: Fail\n"
-	echo 1 >&1
-fi
+tearDown(){
+	# runs after each separate test
+	unset RESULT
+	rm -f _tmp.txt
+	endSkipping
+}
 
-if [ -z "$CLUSTER_ID" ] 
- then
-	echo "Couldn't create cluster"
-	echo "Exiting"
-	return 1 2>/dev/null || exit 1
-fi
+# 01 
+testClusterCreate(){
+	# arrange
+	# act
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		declare -a ARR_RESULT=($(orka create integration_test 2 2 2048 5 2 2048 5 standard escience.grnet.gr 2 128 --use_hadoop_image))
+		CLUSTER_ID=${ARR_RESULT[1]}
+		MASTER_IP=${ARR_RESULT[3]}
+		export SSHPASS=${ARR_RESULT[5]}
+		if [ -n "$MASTER_IP" ]; then
+			HOST=hduser@$MASTER_IP
+			ROOTHOST=root@$MASTER_IP
+			HADOOP_HOME=/usr/local/hadoop
+			sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub $ROOTHOST:/home/hduser/
+			sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
+			'cat /home/hduser/id_rsa.pub >> /home/hduser/.ssh/authorized_keys;
+			rm -f /home/hduser/id_rsa.pub;
+			exit'
+		fi
+	else
+		startSkipping
+	fi
+	# assert (assert* "fail message" <success_condition>)
+	assertTrue 'Create Cluster Failed' '[ -n "$CLUSTER_ID" ]'
+}
 
-# 02 Stop
-RESULT=$(orka hadoop stop $CLUSTER_ID)
-if [ -n "$RESULT" ]; then
-	printf "Stop Hadoop: OK\n"
-	echo 0 >&1
-else
-	printf "Stop Hadoop: Fail\n"
-	echo 1 >&1
-fi
+# 02
+testHadoopStop(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		RESULT=$(orka hadoop stop $CLUSTER_ID)
+	else
+		startSkipping
+	fi
+	assertTrue 'Stop Hadoop Failed' '[ -n "$RESULT" ]'
+}
 
-# 03 Format
-RESULT=$(orka hadoop format $CLUSTER_ID)
-if [ -n "$RESULT" ]; then
-	printf "Format Hadoop: OK\n"
-	echo 0 >&1
-else
-	printf "Format Hadoop: Fail\n"
-	echo 1 >&1
-fi
+# 03
+testHadoopFormat(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		RESULT=$(orka hadoop format $CLUSTER_ID)
+	else
+		startSkipping
+	fi
+	assertTrue 'Format Hadoop Failed' '[ -n "$RESULT" ]'
+}
 
-# 04 Start
-RESULT=$(orka hadoop start $CLUSTER_ID)
-if [ -n "$RESULT" ]; then
-	printf "Start Hadoop: OK\n"
-	echo 0 >&1
-else
-	printf "Start Hadoop: Fail\n"
-	echo 1 >&1
-fi
-
-HOST=hduser@$MASTER_IP
-ROOTHOST=root@$MASTER_IP
-HADOOP_HOME=/usr/local/hadoop
-echo $SSHPASS
-echo $ROOTHOST
-sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub $ROOTHOST:/home/hduser/
-sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-'cat /home/hduser/id_rsa.pub >> /home/hduser/.ssh/authorized_keys;
-rm -f /home/hduser/id_rsa.pub;
-exit'
-echo "Before runPI"
+# 04
+testHadoopRestart(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		RESULT=$(orka hadoop start $CLUSTER_ID)
+	else
+		startSkipping
+	fi
+	assertTrue 'Start Hadoop Failed' '[ -n "$RESULT" ]'
+}
 
 # 05 runPI
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar pi 2 10000' 2>&1 | tee _tmp.txt
-RESULT=$(cat _tmp.txt | grep "Estimated value of Pi is" |cut -d' ' -f6)
-rm -f _tmp.txt
-if [ -n "$RESULT" ]; then
-	printf "Hadoop runPI: OK\n"
-	echo 0 >&1
-else
-	printf "Hadoop runPI: Fail\n"
-	echo 1 >&1
-fi
+testHDFSrunPI(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar pi 2 10000' 2>&1 | tee _tmp.txt
+		RESULT=$(cat _tmp.txt | grep "Estimated value of Pi is" |cut -d' ' -f6)
+	else
+		startSkipping
+	fi
+	assertTrue 'HDFS runPI Failed' '[ -n "$RESULT" ]'
+}
+
+# 06 wordcount
+testHDFSwordcount(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -put /usr/local/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount LICENSE.txt out_wordcount' 2>&1 | tee _tmp.txt
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -test -e out_wordcount/_SUCCESS' > _tmp.txt 2>&1
+		RESULT="$?"
+	else
+		startSkipping
+	fi
+	assertTrue 'HDFS wordcount Failed' '[ "$RESULT" -eq 0 ]'
+}
+
+# 07 teragen
+testHDFSteragen(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar teragen 2684354 out_teragen' 2>&1 | tee _tmp.txt
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -test -e out_teragen/_SUCCESS' > _tmp.txt 2>&1
+		RESULT="$?"
+	else
+		startSkipping
+	fi
+	assertTrue 'HDFS teragen Failed' '[ "$RESULT" -eq 0 ]'
+}
 
 # 08 pithosFS registered
-sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-'/usr/local/hadoop/bin/hdfs dfs -ls pithos://pithos/WordCount/' > _tmp.txt 2>&1
-cat _tmp.txt
-rm -f _tmp.txt
+testRegisteredpithosFS(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -ls pithos://pithos/WordCount/' > _tmp.txt 2>&1
+		# RESULT=$(grep -i "found [0-9]* items" _tmp.txt)
+		RESULT=$(grep -i -c "No FileSystem for scheme" _tmp.txt)
+	else
+		startSkipping
+	fi
+	assertTrue 'pithosFS registration Failed' '[ "$RESULT" -eq 0 ]'
+}
 
-# 12
-RESULT=$(orka destroy $CLUSTER_ID)
-echo "12 Destroy:"$RESULT
-if [ -n "$RESULT" ]; then
-	printf "Destroy Cluster: OK\n"
-	echo 0 >&1
-else
-	printf "Destroy Cluster: Fail\n"
-	echo 1 >&1
-fi
+# 12 Destroy
+testClusterDestroy(){
+	if [ "$DO_INTEGRATION_TEST" = true ]; then
+		RESULT=$(orka destroy $CLUSTER_ID)
+	else
+		startSkipping
+	fi
+	assertTrue 'Destroy Cluster Failed' '[ -n "$RESULT" ]'
+}
 
-tearDown
+. shunit2
