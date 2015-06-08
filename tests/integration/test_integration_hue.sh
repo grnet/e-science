606b8da26d469c971e7b47ed329836a62722d109
gtzelepis <gtzelepis@allweb.gr>
2015-06-08 15:59:04 +0300

    Try cloudera image integration tests, add hue stub

--- tests/integration/test_integration_hue.sh
+++ tests/integration/test_integration_hue.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# file: tests/test_integration_cdh.sh
+# file: tests/test_integration_hue.sh
 
 # shunit2 compatible unittest file, requires shunit2 installed (apt-get install shunit2)
 
@@ -44,19 +44,19 @@ testClusterCreate(){
 	# arrange
 	# act
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
-		# orka create name_of_cluster cluster_size master_cpus master_ram master_disksize slave_cpus slave_ram slave_disksize disk_template project_name replication blocksize
-		declare -a ARR_RESULT=($(orka create integration_test 2 4 4096 5 2 4096 10 standard escience.grnet.gr 2 128 --use_hadoop_image Hadoop_2\.6\.0\-Cloudera\-CDH\-5\.4\.2))
+		# orka create name_of_cluster master_cpus master_ram master_disksize slave_cpus slave_ram slave_disksize disk_template project_name replication blocksize
+		declare -a ARR_RESULT=($(orka create integration_test 2 2 2048 5 2 2048 5 standard escience.grnet.gr 2 128 --use_hadoop_image))
 		CLUSTER_ID=${ARR_RESULT[1]}
 		MASTER_IP=${ARR_RESULT[3]}
 		export SSHPASS=${ARR_RESULT[5]}
 		if [ -n "$MASTER_IP" ]; then
-			HOST=hdfs@$MASTER_IP
+			HOST=hduser@$MASTER_IP
 			ROOTHOST=root@$MASTER_IP
-			HADOOP_HOME=/usr/lib/hadoop
-			sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub $ROOTHOST:/root/id_rsa.pub
+			HADOOP_HOME=/usr/local/hadoop
+			sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub $ROOTHOST:/home/hduser/
 			sshpass -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-			'cat /root/id_rsa.pub >> /root/.ssh/authorized_keys;
-			rm -f /root/id_rsa.pub;
+			'cat /home/hduser/id_rsa.pub >> /home/hduser/.ssh/authorized_keys;
+			rm -f /home/hduser/id_rsa.pub;
 			exit'
 		fi
 	else
@@ -99,8 +99,8 @@ testHadoopRestart(){
 # 05 runPI
 testHDFSrunPI(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/lib/hadoop/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar pi 2 10000' 2>&1 | tee _tmp.txt
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar pi 2 10000' 2>&1 | tee _tmp.txt
 		RESULT=$(cat _tmp.txt | grep "Estimated value of Pi is" |cut -d' ' -f6)
 	else
 		startSkipping
@@ -111,12 +111,12 @@ testHDFSrunPI(){
 # 06 wordcount
 testHDFSwordcount(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/bin/hdfs dfs -put /usr/lib/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/lib/hadoop/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount LICENSE.txt out_wordcount' 2>&1 | tee _tmp.txt
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/bin/hdfs dfs -test -e out_wordcount/_SUCCESS' > _tmp.txt 2>&1
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -put /usr/local/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar wordcount LICENSE.txt out_wordcount' 2>&1 | tee _tmp.txt
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -test -e out_wordcount/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
 	else
 		startSkipping
@@ -127,10 +127,10 @@ testHDFSwordcount(){
 # 07 teragen
 testHDFSteragen(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/lib/hadoop/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen 2684354 out_teragen' 2>&1 | tee _tmp.txt
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/bin/hdfs dfs -test -e out_teragen/_SUCCESS' > _tmp.txt 2>&1
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.5.2.jar teragen 2684354 out_teragen' 2>&1 | tee _tmp.txt
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -test -e out_teragen/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
 	else
 		startSkipping
@@ -141,30 +141,14 @@ testHDFSteragen(){
 # 08 pithosFS registered
 testRegisteredpithosFS(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/bin/hdfs dfs -ls pithos://pithos/WordCount/' > _tmp.txt 2>&1
+		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
+		'/usr/local/hadoop/bin/hdfs dfs -ls pithos://pithos/WordCount/' > _tmp.txt 2>&1
 		# RESULT=$(grep -i "found [0-9]* items" _tmp.txt)
 		RESULT=$(grep -i -c "No FileSystem for scheme" _tmp.txt)
 	else
 		startSkipping
 	fi
 	assertTrue 'pithosFS registration Failed' '[ "$RESULT" -eq 0 ]'
-}
-
-# 10. wordcount pithosFS
-testpithosFSwordcount(){
-	if [ "$DO_INTEGRATION_TEST" = true ]; then
-#		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-#		'/usr/bin/hdfs dfs -put /usr/lib/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/lib/hadoop/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount pithos://pithos/WordCount/warpeace.txt out_pithos_wordcount' 2>&1 | tee _tmp.txt
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ROOTHOST \
-		'/usr/bin/hdfs dfs -test -e out_pithos_wordcount/_SUCCESS' > _tmp.txt 2>&1
-		RESULT="$?"
-	else
-		startSkipping
-	fi
-	assertTrue 'HDFS wordcount Failed' '[ "$RESULT" -eq 0 ]'
 }
 
 # 12 Destroy
