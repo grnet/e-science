826a9c88fc1d4974c69f2a3f58303b490f99d6d4
gtzelepis <gtzelepis@allweb.gr>
2015-06-15 12:35:35 +0300

    Suppress debug output from integration tests

--- tests/integration/test_integration_hdpbase.sh
+++ tests/integration/test_integration_hdpbase.sh
@@ -20,7 +20,7 @@
 oneTimeSetUp(){
 	# runs before whole test suite
 	if [ -z "${STAGING_IP}" ]; then
-		STAGING_IP=http://83.212.116.40
+		STAGING_IP=http://83.212.115.45
 	fi
 	local OKEANOS_TOKEN=$(cat .private/.config.txt | grep "token" |cut -d' ' -f3)
 	echo -e '[global]\ndefault_cloud = ~okeanos\nignore_ssl = on\n[cloud "~okeanos"]\nurl = https://accounts.okeanos.grnet.gr/identity/v2.0\ntoken = '$OKEANOS_TOKEN'\n[orka]\nbase_url = '$STAGING_IP > ~/.kamakirc	
@@ -47,7 +47,7 @@ testClusterCreate(){
 	# act
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
 		# orka create name_of_cluster size_of_cluster master_cpus master_ram master_disksize slave_cpus slave_ram slave_disksize disk_template project_name replication blocksize
-		declare -a ARR_RESULT=($(orka create hdp_integration_test 2 4 4096 5 2 4096 10 standard escience.grnet.gr 2 128 --use_hadoop_image Hadoop\-2\.5\.2\-Debian\-8\.0))
+		declare -a ARR_RESULT=($(orka create hdp_integration_test 2 4 4096 5 2 4096 10 standard escience.grnet.gr 2 128 --use_hadoop_image Hadoop\-2\.5\.2\-Debian\-8\.0 2> _tmp.txt))
 		CLUSTER_ID=${ARR_RESULT[1]}
 		MASTER_IP=${ARR_RESULT[3]}
 		export SSHPASS=${ARR_RESULT[5]}
@@ -102,8 +102,9 @@ testHadoopRestart(){
 testHDFSrunPI(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar pi 2 10000' 2>&1 | tee _tmp.txt
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar pi 2 10000' > _tmp.txt 2>&1
 		RESULT=$(cat _tmp.txt | grep "Estimated value of Pi is" |cut -d' ' -f6)
+		echo $RESULT
 	else
 		startSkipping
 	fi
@@ -116,7 +117,7 @@ testHDFSwordcount(){
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 		'/usr/local/hadoop/bin/hdfs dfs -put /usr/local/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar wordcount LICENSE.txt out_wordcount' 2>&1 | tee _tmp.txt
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar wordcount LICENSE.txt out_wordcount' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 		'/usr/local/hadoop/bin/hdfs dfs -test -e out_wordcount/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
@@ -130,7 +131,7 @@ testHDFSwordcount(){
 testHDFSteragen(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar teragen 2684354 out_teragen' 2>&1 | tee _tmp.txt
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar teragen 2684354 out_teragen' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 		'/usr/local/hadoop/bin/hdfs dfs -test -e out_teragen/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
@@ -159,7 +160,7 @@ testpithosFSwordcount(){
 #		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 #		'/usr/local/hadoop/bin/hdfs dfs -put /usr/lib/hadoop/LICENSE.txt LICENSE.txt' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar wordcount pithos://pithos/WordCount/warpeace.txt out_pithos_wordcount' 2>&1 | tee _tmp.txt
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar wordcount pithos://pithos/WordCount/warpeace.txt out_pithos_wordcount' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 		'/usr/local/hadoop/bin/hdfs dfs -test -e out_pithos_wordcount/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
@@ -173,7 +174,7 @@ testpithosFSwordcount(){
 testpithosFSteragen(){
 	if [ "$DO_INTEGRATION_TEST" = true ]; then
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
-		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar teragen 1342177 pithos://pithos/out_teragen/' 2>&1 | tee _tmp.txt
+		'/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar teragen 1342177 pithos://pithos/out_teragen/' > _tmp.txt 2>&1
 		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $HOST \
 		'/usr/local/hadoop/bin/hdfs dfs -test -e pithos://pithos/out_teragen/_SUCCESS' > _tmp.txt 2>&1
 		RESULT="$?"
