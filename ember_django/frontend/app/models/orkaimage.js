914fd212ea968b9868b24e82b83ab23fb2185da2
gtzelepis <gtzelepis@allweb.gr>
2015-07-13 10:01:28 +0300

    Manually sync with KPetsas/es388 and nvrionis/develop for es48 functionalities, resolved conflicts, fixed some artifacts and changed some labels and menu placement for consistenncy

--- ember_django/frontend/app/models/orkaimage.js
+++ ember_django/frontend/app/models/orkaimage.js
@@ -4,9 +4,9 @@ App.Orkaimage = DS.Model.extend({
     image_name : attr('string'), // OrkaImage name
     image_pithos_uuid : attr('string'), // Linked Pithos Image UUID
     image_components : attr('string'), // Stringified OrkaImage Components metadata (json.dumps)
-    active_image : function() {// TODO: see if we can avoid hardcoding
+    active_image : function() {
         var name = this.get('image_name');
-        if (name == 'Cloudera-CDH-5.4.2') {
+        if (name == 'Debian Base') {
             return true;
         } else {
             return false;
