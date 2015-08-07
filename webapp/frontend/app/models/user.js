a559d8866cfe209ed30b90ebf497b5644571a89f
gtzelepis <gtzelepis@allweb.gr>
2015-08-07 20:33:18 +0300

    Refactor vre_access_url computed property to make it easier to add VRE access ports, use it to add DSpace port.

--- webapp/frontend/app/models/user.js
+++ webapp/frontend/app/models/user.js
@@ -43,7 +43,15 @@ App.Uservreserver = DS.Model.extend({
     // computed properties
     vre_access_url : function(){
         // TODO: add to components info and resolve dynamically
-        return (this.get('os_image')=='Redmine-3.0.4') && 'http://%@:%@'.fmt(this.get('server_IP'),'10083') || 'http://%@'.fmt(this.get('server_IP'));
+        var image = this.get('os_image');
+        switch (image){
+        case 'Redmine-3.0.4':
+            return 'http://%@:%@'.fmt(this.get('server_IP'),'10083');
+        case 'DSpace-5.3':
+            return 'http://%@:%@'.fmt(this.get('server_IP'),'8080');
+        default:
+            return 'http://%@'.fmt(this.get('server_IP'));
+        }
     }.property('server_IP','os_image'),
     class_vre_status : function (){
         var status = this.get('server_status');
