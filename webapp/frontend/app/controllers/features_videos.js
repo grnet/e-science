b47bca301b97ade76059da06d3bbe8c75c8b7e0c
gtzelepis <gtzelepis@allweb.gr>
2015-10-31 19:22:48 +0200

    Add ability to configure Videos per row in Features from admin AppSettings. Fallback to a default of 2 if not found.

--- webapp/frontend/app/controllers/features_videos.js
+++ webapp/frontend/app/controllers/features_videos.js
@@ -1,6 +1,11 @@
 // Features/videos controller
 App.FeaturesVideosController = Ember.Controller.extend({
+	needs : 'application',
 	STATIC_URL : DJANGO_STATIC_URL,
+	videos_per_row : function(key,value){
+	    var video_settings = this.get('AppSettings').filterBy('section','Features').filterBy('property_name','Videos');
+        return Ember.isEmpty(video_settings) ? null : JSON.parse(video_settings[0].get('property_value'))[key];
+	}.property(),
 	videos_by_row : function(){
 	    /*
          *  [{'row':row_index, 'colspec': 'col-sm-x', 'videos':[item1,item2,item3,..]},...]
@@ -9,7 +14,7 @@ App.FeaturesVideosController = Ember.Controller.extend({
         var result = [];
         var content = this.get('content').sortBy('video_aspect');
         var row_index = null;
-        var vids_per_row = 2;
+        var vids_per_row = Ember.isEmpty(this.get('videos_per_row')) ? 2 : Number(this.get('videos_per_row'));
         var bootstrap_cols = ((12/vids_per_row) | 0);
         content.forEach(function(item,index) {
             row_index = ((index/vids_per_row) | 0);
